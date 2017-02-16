08c9362d01a8e72c7804d54a27b15a9cd75b5d7b
php 33 <php.33@metralha.cec.ime.usp.br>
2017-02-16 19:32:58 -0200

    acrescentando listagem e detalhes da editora

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/editoras', 'EditoraController@index');
+Route::get('/editoras/{editora}', 'EditoraController@show');
